00f1a102938df054ed3f3a501f2af8def68dec82
Marco Koch <marco.koch@uni-saarland.de>
2020-11-10 22:42:52 +0100

    Added UserTable and started layout

---
+++ src/components/UserControl/UserTable.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-page class="col items-center root">
+    <q-table
+      title="User Panel"
+      :data="mockdata"
+      :columns="columns"
+      row-key="name"
+      class="usertable"
+    ></q-table>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class UserTable extends Vue {
+  columns = [
+    {
+      name: "name",
+      required: true,
+      label: "User name",
+      align: "left",
+      field: "name",
+      sortable: true
+    },
+    {
+      name: "group",
+      align: "center",
+      label: "Access Group",
+      field: "accessGroup",
+      sortable: true
+    },
+    {
+      name: "reservedMedia",
+      label: "Reserved Media",
+      field: "reservedMedia",
+      sortable: true
+    },
+    {
+      name: "borrowedMedia",
+      label: "Borrowed Media",
+      field: "borrowedMedia",
+      sortable: true
+    }
+  ];
+  mockdata = [
+    {
+      name: "user1",
+      accessGroup: 1,
+      token: "string",
+      //0 = guest; 1 = registered user; 2 = staff; 3 = admin
+      reservedMedia: 1,
+      borrowedMedia: 0
+    },
+    {
+      name: "user2",
+      accessGroup: 2,
+      token: "string",
+      //0 = guest; 1 = registered user; 2 = staff; 3 = admin
+      reservedMedia: 0,
+      borrowedMedia: 2
+    },
+    {
+      name: "user3",
+      accessGroup: 3,
+      token: "string",
+      //0 = guest; 1 = registered user; 2 = staff; 3 = admin
+      reservedMedia: 0,
+      borrowedMedia: 0
+    }
+  ];
+}
+</script>
+
+<style scoped lang="scss">
+.root {
+  width: 100%;
+}
+.usertable {
+  width: 100%;
+  max-width: 1000px;
+}
+</style>
--- src/pages/UserControl.vue
+++ src/pages/UserControl.vue
@@ -1,11 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <UserTable />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import UserTable from "components/UserControl/UserTable.vue";
 
-@Component
+@Component({
+  components: { UserTable }
+})
 export default class UserControl extends Vue {}
 </script>
 
