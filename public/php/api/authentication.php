3121a79250834a0f1e92bdf1a27a780ac5ef321a
Marco Koch <marco.koch@uni-saarland.de>
2021-02-19 16:58:34 +0100

    basic authenticator api

---
+++ public/php/api/authentication.php
@@ -0,0 +1,35 @@
+<?php
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$postData = json_decode(file_get_contents("php://input"),true);
+$call = isset($postData['call']) ? $postData['call'] : '';
+$payload = isset($postData['payload']) ? $postData['payload'] : '';
+require_once '../config.php';
+require_once '../classes/authentication.php';
+$auth = new Authenticator();
+
+switch ($requestMethod) {
+    case 'POST':
+        switch ($call) {
+            case 'login':
+                $user = isset($payload["username"]) ? $payload["username"] : "";
+                $password = isset($payload["password"]) ? $payload["password"] : "";
+                $remember = isset($payload["username"]) ? $payload["username"] : "false";
+                $auth->setUser($user,$password, $remember);
+                $auth->login();
+                break;
+            case 'remember':
+
+                break;
+            default:
+                # code...
+                break;
+        }
+        break;
+    case 'DELETE':
+        // $userGateway->deleteUser("test");
+        break;
+    default:
+        // header("HTTP/1.1 404 Not Found");
+        // exit();
+        break;
+}
---
+++ public/php/classes/authentication.php
@@ -0,0 +1,101 @@
+<?php
+require_once '../config.php';
+require_once 'user.php';
+
+$auth = new Authenticator();
+$auth->rememberMe();
+// $auth = new Authenticator("mako017", "LibrAdmin", "true");
+// $auth->login();
+
+class Authenticator {
+    private string $SECRET_KEY = "015514d3f7df22eec03193d0a0df8e53";
+    private string $TOKEN_TABLE = "logintokens";
+
+    private string $userName;
+    private string $password;
+    private bool $keepMe;
+
+    public function setUser(string $userName, string $password, string $keepMe = "false") {
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->keepMe = ($keepMe==="true") ? true : false;
+    }
+
+    private function generateRandomToken(){
+        try {
+            $string = random_bytes(16);
+        } catch (Exception $e) {
+            die("Could not generate a random string. Is our OS secure?");
+        }
+        return bin2hex($string);
+    }
+
+    private function storeToken($token)
+    {
+        if($this->fetchTokenByUserName() !== NULL){
+            DB::delete($this->TOKEN_TABLE, "userName=%s", $this->userName);
+        } 
+        DB::insert($this->TOKEN_TABLE,[
+            "userName" => $this->userName,
+            "token" => $token
+        ]);
+
+    }
+
+    private function fetchTokenByUserName()
+    {
+        return DB::queryFirstField("SELECT `token` FROM `" . $this->TOKEN_TABLE . "` WHERE `userName` = %s", $this->userName);
+    }
+
+    private function authentifyUser()
+    {
+        $userGate = new UserGateway();
+        $user = $userGate->readSingleUser($this->userName);
+        var_dump($user);
+    }
+
+    public function login()
+    {
+        $result = DB::queryFirstRow('SELECT `password` FROM `users` WHERE `name` = %s', $this->userName);
+        if (!password_verify($this->password, $result["password"])){
+            echo("not authentified");
+            return;
+        }
+        else{
+            $this->authentifyUser();
+            if ($this->keepMe) {
+                $this->keepLogin();
+            }
+        }
+    }
+
+    public function logout(){
+        setcookie("rememberme", "", time() - 3600);
+    }
+
+    public function keepLogin()
+    {
+        $token = $this->generateRandomToken();
+        $this->storeToken($token);
+        $cookie = $this->userName . ':' . $token;
+        $mac = hash_hmac('sha256', $cookie, $this->SECRET_KEY);
+        $cookie .= ':' . $mac;
+        setcookie('rememberme', $cookie, time()+60*60*24*14,"","", true, true);
+    }
+
+    public function rememberMe() 
+    {
+        $cookie = isset($_COOKIE['rememberme']) ? $_COOKIE['rememberme'] : '';
+        if ($cookie) {
+            list($user, $token, $mac) = explode(':', $cookie);
+            $this->userName = $user;
+            if (!hash_equals(hash_hmac('sha256', $user . ':' . $token, $this->SECRET_KEY), $mac)) {
+                return false;
+            }
+            $usertoken = $this->fetchTokenByUserName($user);
+            if (hash_equals($usertoken, $token)) {
+                $this->authentifyUser();
+            }
+        }
+    }
+}
