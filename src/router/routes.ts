dcffdd100e8719b434ef2e445428e4000cfe0951
Marco Koch <marco.koch@uni-saarland.de>
2021-02-22 12:52:01 +0100

    prettier

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,37 +4,27 @@ const routes: RouteConfig[] = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/Index.vue") }
-    ]
+    children: [{ path: "", component: () => import("pages/Index.vue") }]
   },
   {
     path: "/usercontrol",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/UserControl.vue") }
-    ]
+    children: [{ path: "", component: () => import("pages/UserControl.vue") }]
   },
   {
     path: "/catalogue",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/Catalogue.vue") }
-    ]
+    children: [{ path: "", component: () => import("pages/Catalogue.vue") }]
   },
   {
     path: "/qrscanner",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/QR.vue") }
-    ]
+    children: [{ path: "", component: () => import("pages/QR.vue") }]
   },
   {
     path: "/login",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/UserLogin.vue") }
-    ]
+    children: [{ path: "", component: () => import("pages/UserLogin.vue") }]
   },
   // Always leave this as last one,
   // but you can also remove it
