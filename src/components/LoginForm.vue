263f075c65222245e93075988c84426603485f71
Marco Koch <marco.koch@uni-saarland.de>
2021-02-22 12:42:21 +0100

    moved login to vuex

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,21 +2,30 @@
   <q-form @submit="login" class="col items-center justify-evenly">
     <q-input v-model="credentials.username" label="Username" />
     <q-input v-model="credentials.password" type="password" label="Password" />
+    <q-checkbox
+      v-model="credentials.rememberMe"
+      label="Stay Logged in (uses cookies)"
+      true-value="true"
+      false-value="false"
+    />
     <q-btn label="Submit" type="submit" color="primary" class="q-mt-sm" />
   </q-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { user } from "src/store";
+import { UserCredentials } from "./models";
 
 @Component
 export default class LoginForm extends Vue {
-  credentials = {
+  credentials: UserCredentials = {
     username: "",
-    password: ""
+    password: "",
+    rememberMe: "false"
   };
   login() {
-    return 1;
+    user.loginUser(this.credentials);
   }
 }
 </script>
