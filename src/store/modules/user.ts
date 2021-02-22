46ab50d95a8f2dd557edeb09ae5931f5c0f73948
Marco Koch <marco.koch@uni-saarland.de>
2021-02-22 12:42:03 +0100

    updated user state

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,33 +1,58 @@
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 import store from "../";
+import axios from "axios";
+import {
+  serverResponse,
+  ServerResponseUser,
+  UserCredentials
+} from "src/components/models";
 
-@Module({ name: "files", namespaced: true, store })
-export default class Files extends VuexModule {
-  private _name = "";
+@Module({ name: "user", namespaced: true, store })
+export default class User extends VuexModule {
+  private readonly USER_API = "user.php";
+  private readonly AUTH_API = "authentication.php";
+  private _name: string | undefined = undefined;
+  private _role: string | undefined = undefined;
+  private _loggedIn = false;
 
   get name() {
     return this._name;
   }
 
-  //   @Action
-  //   readEvaplan(path: string) {
-  //     const workbook = new exceljs.Workbook();
-  //     fs.promises
-  //       .readFile(path)
-  //       .then(data => {
-  //         workbook.xlsx
-  //           .load(data.buffer)
-  //           .then(() => {
-  //             const ep = new EvaPlan(workbook.getWorksheet("Tabelle1"));
-  //             this.setEvaplan(ep);
-  //           })
-  //           .catch(err => console.error(err));
-  //       })
-  //       .catch(err => console.error(err));
-  //   }
+  @Action
+  loginUser(credentials: UserCredentials) {
+    axios
+      .post(
+        "http://localhost/libradmin/php/api/" + this.AUTH_API,
+        JSON.stringify({
+          call: "login",
+          payload: credentials
+        })
+      )
+      .then(response => {
+        const data: serverResponse = response.data as serverResponse;
+        if (data?.call === "login") {
+          const user: ServerResponseUser = data.payload as ServerResponseUser;
+          this.setName(user.name);
+          this.setRole(user.role);
+          this.setLogin(true);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
 
   @Mutation
   setName(name: string) {
     this._name = name;
+  }
+  @Mutation
+  setRole(role: string) {
+    this._role = role;
+  }
+  @Mutation
+  setLogin(status: boolean) {
+    this._loggedIn = status;
   }
 }
