77e4ce86aff256f6a2ccdf8da1f82f7f88633a28
Marco Koch <marco.koch@uni-saarland.de>
2021-04-01 15:10:42 +0200

    +popupEdit for admin and update call

--- src/components/Catalogue/ItemTable.vue
+++ src/components/Catalogue/ItemTable.vue
@@ -6,17 +6,134 @@
       :columns="columns"
       :rows-per-page-options="[0]"
       row-key="name"
-    ></q-table>
+    >
+      <template v-slot:body="props" v-if="ability.can('manage', 'tests')">
+        <q-tr :props="props">
+          <q-td key="ID" :props="props">
+            {{ props.row.itemID }}
+            <q-popup-edit
+              v-model="popupEditData.itemID"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.itemID" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="status" :props="props">
+            {{ props.row.status }}
+            <q-popup-edit
+              v-model="popupEditData.status"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.status" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="title" :props="props">
+            {{ props.row.title }}
+            <q-popup-edit
+              v-model="popupEditData.title"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.title" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="authors" :props="props">
+            {{ props.row.authors }}
+            <q-popup-edit
+              v-model="popupEditData.authors"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.authors" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="category1" :props="props">
+            {{ props.row.category1 }}
+            <q-popup-edit
+              v-model="popupEditData.category1"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.category1" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="category2" :props="props">
+            {{ props.row.category2 }}
+            <q-popup-edit
+              v-model="popupEditData.category2"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.category2" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="category3" :props="props">
+            {{ props.row.category3 }}
+            <q-popup-edit
+              v-model="popupEditData.category3"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.category3" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="category4" :props="props">
+            {{ props.row.category4 }}
+            <q-popup-edit
+              v-model="popupEditData.category4"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.category4" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="publisher" :props="props">
+            {{ props.row.publisher }}
+            <q-popup-edit
+              v-model="popupEditData.publisher"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.publisher" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="language" :props="props">
+            {{ props.row.language }}
+            <q-popup-edit
+              v-model="popupEditData.language"
+              @show="() => setEditData(props.row)"
+              @save="val => saveChange()"
+              buttons
+            >
+              <q-input v-model="popupEditData.language" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { CatalogueItem } from "components/models";
-import { catalogue } from "src/store/";
+import { catalogue, user } from "src/store/";
+import { emptyCatalogueItem } from "assets/ts/initFunctions";
 
 @Component({})
 export default class ItemTable extends Vue {
+  popupEditData: CatalogueItem = emptyCatalogueItem();
   columns = [
     {
       name: "ID",
@@ -101,6 +218,16 @@ export default class ItemTable extends Vue {
   ];
   get allItems() {
     return catalogue.allItems;
+  }
+  get ability() {
+    return user.ability;
+  }
+  saveChange() {
+    catalogue.updateItem(this.popupEditData);
+  }
+  setEditData(data: CatalogueItem) {
+    this.popupEditData = { ...data };
+    console.log(this.popupEditData);
   }
 }
 </script>
--- src/store/modules/catalogue.ts
+++ src/store/modules/catalogue.ts
@@ -3,6 +3,11 @@ import store from "..";
 import axios from "axios";
 import { CatalogueItem, serverResponse } from "src/components/models";
 
+interface updateCommit {
+  updatedItem: CatalogueItem;
+  pos: number;
+}
+
 @Module({ name: "catalogue", namespaced: true, store })
 export default class Catalogue extends VuexModule {
   private readonly API_PATH = process.env.API_BASE_URL as string;
@@ -26,8 +31,34 @@ export default class Catalogue extends VuexModule {
       .catch(err => console.error(err));
   }
 
+  @Action
+  updateItem(updatedItem: CatalogueItem) {
+    const pos = this._allItems.findIndex(
+      item => item.catalogueCounter === updatedItem.catalogueCounter
+    );
+    if (pos !== -1) {
+      this.setCatalogueItem({ updatedItem, pos });
+      axios
+        .put(
+          this.API_PATH + this.API,
+          encodeURIComponent(
+            JSON.stringify({ call: "", payload: updatedItem })
+          ),
+          { method: "PUT" }
+        )
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
   @Mutation
   setCatalogue(newCatalogue: CatalogueItem[]) {
     this._allItems = newCatalogue;
+  }
+  @Mutation
+  setCatalogueItem({ updatedItem, pos }: updateCommit) {
+    this._allItems.splice(pos, 1, updatedItem);
   }
 }
