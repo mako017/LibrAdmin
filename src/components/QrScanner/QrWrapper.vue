84d27312395209bb7aecc0aa8ca72cbc68ef5112
Marco Koch <marco.koch@uni-saarland.de>
2021-09-13 10:48:17 +0200

    prevent continuous scanning

--- src/components/QrScanner/QrWrapper.vue
+++ src/components/QrScanner/QrWrapper.vue
@@ -9,13 +9,24 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class QrWrapper extends Vue {
   qrScanner: QrScanner | undefined;
+  scannedContent = "";
+  allowRescan = false;
   mounted() {
     const videoEl = document.getElementById("qrVid") as HTMLVideoElement;
     console.log(videoEl);
 
     this.qrScanner = new QrScanner(videoEl, result => {
-      this.$emit("scanned", result);
+      if (!result.trim()) {
+        return;
+      }
+      if (this.scannedContent !== result || this.allowRescan) {
+        this.scannedContent = result;
+        this.$emit("scanned", result);
+        this.allowRescan = false;
+        setInterval(() => (this.allowRescan = true), 2000);
+      }
     });
+
     if (this.qrScanner) {
       void this.qrScanner.start();
     }
