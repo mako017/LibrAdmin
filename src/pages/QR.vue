a08cb980f880a1864119e4585522896f021860e7
marco Koch <marco.koch@uni-saarland.de>
2021-09-14 14:21:23 +0200

    enabled qr actions

--- src/pages/QR.vue
+++ src/pages/QR.vue
@@ -55,8 +55,23 @@ export default class QR extends Vue {
   setContent(content: string) {
     const urlParts = content.split("/");
     this.qrContent = urlParts.pop() || "";
+    if (this.qrContent) {
+      switch (this.qrAction) {
+        case QrActions.return:
+          this.returnMedia(this.qrContent);
+          break;
+        case QrActions.inspect:
+          this.inspectMedia(this.qrContent);
+          break;
+        case QrActions.borrow:
+          this.borrowMedia(this.qrContent);
+          break;
+        case QrActions.reserve:
+          this.reserveMedia(this.qrContent);
+          break;
+      }
+    }
     if (this.qrAction === QrActions.return && this.qrContent) {
-      this.returnMedia(this.qrContent);
     }
   }
   returnMedia(mediaID: string) {
@@ -73,10 +88,24 @@ export default class QR extends Vue {
     void this.$router.push("/catalogue/" + mediaID);
   }
   borrowMedia(mediaID: string) {
-    borrowItem(mediaID, this.selectedUser);
+    Dialog.create({
+      title: "Confirm",
+      message: `Lend this test to ${this.selectedUser}?`,
+      cancel: true,
+      persistent: true
+    }).onOk(() => {
+      borrowItem(mediaID, this.selectedUser);
+    });
   }
   reserveMedia(mediaID: string) {
-    reserveItem(mediaID, this.selectedUser);
+    Dialog.create({
+      title: "Confirm",
+      message: `Block this test for ${this.selectedUser}?`,
+      cancel: true,
+      persistent: true
+    }).onOk(() => {
+      reserveItem(mediaID, this.selectedUser);
+    });
   }
 }
 </script>
